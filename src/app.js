b46cfbea632354b46588b2778e66b9cbc56b3bcd
jdiaz1993 <diazjoseph366@gmail.com>
2025-03-01 17:54:12 +0000

    set object and label

--- src/app.js
+++ src/app.js
@@ -5,25 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
- document.querySelector("#newCard").addEventListener("click", randomCardGenerator )
- 
+window.onload = function () {
+  document.querySelector("#newCard").addEventListener("click", randomCardGenerator);
+  randomCardGenerator()
+
 }
 
 const randomCardGenerator = () => {
 
- 
 
-  let suits = ["♥", "♦", "♠", "♣"]
-  let cardNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+  let suits = [{label:"♥", color:"red"}, {label:"♦", color:"red"}, {label:"♠", color: "black"}, {label:"♣", color: "black"}]
+  let cardNumbers = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)]
+  let randomNumber = cardNumbers[Math.floor(Math.random() * cardNumbers.length)]
+
+  const suitColors = document.querySelectorAll(".suit")
+
+
+  //document.querySelector("#top-suit").innerHTML = randomSuit.label;
+  document.querySelector("#card-number").innerHTML = randomNumber;
+  //document.querySelector("#bottom-suit").innerHTML = randomSuit.label;
+
+console.log(suitColors)
+  for (const suit of suitColors) {
+    suit.innerHTML = randomSuit.label
+    suit.style.color = randomSuit.color
    
+  }
 
-  document.querySelector("#top-suit").innerHTML = suits[Math.floor(Math.random() * suits.length)];
-  document.querySelector("#card-number").innerHTML = cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
-  document.querySelector("#bottom-suit").innerHTML = suits[Math.floor(Math.random() * suits.length)];
-  
-  
- 
- 
+
 
 }
