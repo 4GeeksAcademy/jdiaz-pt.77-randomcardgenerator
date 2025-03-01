83ebeff790e7b975e076f6de2e043ab7135578d0
jdiaz1993 <diazjoseph366@gmail.com>
2025-03-01 06:10:13 +0000

    refactored code

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+ document.querySelector("#newCard").addEventListener("click", randomCardGenerator )
+ 
+}
+
+const randomCardGenerator = () => {
+
+ 
+
+  let suits = ["♥", "♦", "♠", "♣"]
+  let cardNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+   
+
+  document.querySelector("#top-suit").innerHTML = suits[Math.floor(Math.random() * suits.length)];
+  document.querySelector("#card-number").innerHTML = cardNumbers[Math.floor(Math.random() * cardNumbers.length)];
+  document.querySelector("#bottom-suit").innerHTML = suits[Math.floor(Math.random() * suits.length)];
+  
+  
+ 
+ 
+
+}
